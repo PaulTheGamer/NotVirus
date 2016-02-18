beb7248001e701c8c8a627f0439654cb1790ad28
PaulTheGamer <minecraftlover457@gmail.com>
2016-02-18 22:34:37 +0100

    The coding
    Of the "Not Virus"

---
+++ NotVirus.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading;
+
+namespace NotVirus
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Title = Hai there p;
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine (Hey there fucker!);
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine ();
+			Console.WriteLine (Totally not deleting C);
+			Console.Beep(658, 125);
+			Console.Beep(1320, 500);
+			Console.Beep(990, 250);
+			Console.Beep(1056, 250);
+			Console.WriteLine (Totally not overwriting MBR);
+			Console.Beep(1188, 250);
+			Console.Beep(1320, 125);
+			Console.Beep(1188, 125);
+			Console.Beep(1056, 250);
+			Console.Beep(990, 250);
+			Console.WriteLine (Totally not installing Keylogger to BIOS);
+			Console.Beep(880, 500);
+			Console.Beep(880, 250);
+			Console.Beep(1056, 250);
+			Console.WriteLine (Totally not launching Rootkit);
+			Console.Beep(1320, 500);
+			Console.Beep(1188, 250);
+			Console.Beep(1056, 250);
+			Console.WriteLine (Totally not stealing passwords);
+			Console.Beep(990, 750);
+			Console.Beep(1056, 250);
+			Console.WriteLine (Totally not faceraping);
+			Console.Beep(1188, 500);
+			Console.Beep(1320, 500);
+			Console.WriteLine (Totally not planting illegal shit);
+			Console.Beep(1056, 500);
+			Console.Beep(880, 500);
+			Console.WriteLine (Totally not done now p);
+			Console.Beep(880, 500);
+		}
+	}
+}
